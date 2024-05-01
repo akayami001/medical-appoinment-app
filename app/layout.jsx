ecc64b4e5137d3534ca31a7f068cbdd9d8131122
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-05-01 13:44:40 +0100

    header is created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
