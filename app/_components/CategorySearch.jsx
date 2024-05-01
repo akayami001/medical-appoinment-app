4c771191b52c3720d151331e0227de2bd944f43b
Elif Alinli <semraelifalinli@gmail.com>
2024-05-01 14:14:14 +0100

    create the catg. search component

---
+++ app/_components/CategorySearch.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+
+const CategorySearch = () => {
+  return (
+    <div className="mb-10 items-center flex flex-col gap-2">
+      <h2 className="font-bold text-4xl tracking-wide">
+        Search <span className="text-primary">Doctors</span>
+      </h2>
+      <h2 className="text-gray-500 text-xl">
+        Search your Doctor and Book Appointment in one click
+      </h2>
+      <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="Search..." />
+        <Button type="submit">
+          <Search className="h-4 w-4 mr-2" />
+          Search
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CategorySearch;
