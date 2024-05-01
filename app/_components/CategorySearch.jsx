41cd91083363dc15851cb5eabc19045c3cf9d991
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-05-01 18:44:15 +0100

    Categories completed

---
+++ app/_components/CategorySearch.jsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import { data } from "../_mocks_/CategoryData";
+import Image from "next/image";
+
+const CategorySearch = () => {
+  return (
+    <div className="mb-10 items-center flex flex-col gap-4 ">
+      <h2 className="font-bold text-4xl tracking-wide">
+        Search <span className="text-primary">Doctors</span>
+      </h2>
+      <h2 className="text-gray-500 text-xl">
+        Search Your Doctor and Book Appointment
+      </h2>
+      <div className="flex w-full mt-3 max-w-sm items-center space-x-2 ">
+        <Input type="text" placeholder="Search..." />
+        <Button type="submit">
+          <Search className="h-4 w-4 mr-2" />
+          Search
+        </Button>
+      </div>
+      <div className="grid grid-cols-3  ">
+        {data.map((item) => (
+          <div
+            key={item.id}
+            className="flex flex-col text-center justify-center items-center p-5 bg-blue-50 m-2 rounded-lg gap-10 hover:scale-110 transition-all ease-in-out"
+          >
+            <Image
+              src={item.url}
+              alt="icon"
+              width={70}
+              height={60}
+              className="bg-blue-50"
+            />
+            <label className="text-blue-600 text-sm"> {item.name}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategorySearch;
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import CategorySearch from "./_components/CategorySearch";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="">Home</h1>
+      <CategorySearch />
     </main>
   );
 }
