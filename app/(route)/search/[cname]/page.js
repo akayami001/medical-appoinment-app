f32de18ed328d288ec4b5837f12222503c72a249
fatihyanik <fatihyanik07@gmail.com>
2024-05-06 08:44:35 +0200

    added doctor by category

---
+++ app/(route)/search/[cname]/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+const Search = ({ params }) => {
+
+
+    return (
+        <div className='mt-5'>
+           
+        </div>
+    )
+}
+
+export default Search
---
+++ app/(route)/search/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CategoryList from './_components/CategoryList'
+
+const Layout = ({ children }) => {
+    return (
+        <div className='grid grid-cols-4'>
+            <div className='hidden md:block'>
+                <CategoryList />
+            </div>
+            <div className='col-span-4 md:col-span-3'>{children}</div>
+        </div>
+    )
+}
+
+export default Layout
