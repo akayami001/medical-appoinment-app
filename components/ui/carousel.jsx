41a45cca8931b37e596013c1f8349e89ead3ae23
Semra Elif <121907413+elifalinli@users.noreply.github.com>
2024-05-10 14:32:42 +0100

    Update carousel.jsx

--- components/ui/carousel.jsx
+++ components/ui/carousel.jsx
@@ -2,7 +2,6 @@
 import * as React from "react"
 import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
