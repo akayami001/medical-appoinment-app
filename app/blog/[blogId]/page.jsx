d841c2209e46bda33af8fe87cbc5dfa895d27a55
herrbaha <baha_i@hotmail.com>
2024-05-20 23:50:07 +0200

    strapi verbindung zum frontend gemacht

--- app/blog/[blogId]/page.jsx
+++ app/blog/[blogId]/page.jsx
@@ -1,37 +1,78 @@
 "use client";
-import { blogPosts } from "@/app/_mocks_/BlogsData";
+// import { blogPosts } from "@/app/_mocks_/BlogsData";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import useFetch from "@/app/hooks/useFetch";
 
 const BlogDetails = ({ params }) => {
   const { blogId } = params;
-  const post = blogPosts.find((post) => post.id.toString() === blogId);
+  // const post = blogPosts.find((post) => post.id.toString() === blogId);
+  const { isLoading, error, data } = useFetch(
+    `http://localhost:1337/api/blogs/${blogId}`
+  );
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      setPost(data.data);
+    }
+  }, [data]);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error: {error.message}</h1>;
 
   if (!post) {
     return <div>No such blog post found.</div>;
   }
 
+  const { attributes } = post;
+
   return (
+
     <div className="flex justify-center items-center">
       <div className="w-full max-w-md">
         <div className="overflow-hidden rounded-lg shadow transition hover:shadow-lg flex flex-col items-center justify-center p-4">
-          <Image
-            alt={post.title}
-            src={post.image}
-            width={270}
-            height={100}
-            priority
-            className="rounded-t-lg mx-auto"
-          />
-          <time dateTime={post.date} className="block text-xs text-gray-500">
-            {post.date}
+          {attributes.image && (
+            <Image
+              alt={attributes.BlogTitle}
+              src={attributes.image}
+              width={270}
+              height={100}
+              priority
+              className="rounded-t-lg mx-auto"
+            />
+          )}
+          <time dateTime={attributes.BlogDate} className="block text-xs text-gray-500">
+            {new Date(attributes.BlogDate).toLocaleDateString()}
           </time>
           <h3 className="mt-2 text-lg font-semibold text-gray-900">
-            {post.title}
+            {attributes.BlogTitle}
           </h3>
-          <p className="mt-2 text-sm text-gray-500">{post.content}</p>
+          <p className="mt-2 text-sm text-gray-500">{attributes.BlogText}</p>
         </div>
       </div>
     </div>
+    // <div className="flex justify-center items-center">
+    //   <div className="w-full max-w-md">
+    //     <div className="overflow-hidden rounded-lg shadow transition hover:shadow-lg flex flex-col items-center justify-center p-4">
+    //       <Image
+    //         alt={post.title}
+    //         src={post.image}
+    //         width={270}
+    //         height={100}
+    //         priority
+    //         className="rounded-t-lg mx-auto"
+    //       />
+    //       <time dateTime={post.date} className="block text-xs text-gray-500">
+    //         {post.date}
+    //       </time>
+    //       <h3 className="mt-2 text-lg font-semibold text-gray-900">
+    //         {post.title}
+    //       </h3>
+    //       <p className="mt-2 text-sm text-gray-500">{post.content}</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,43 +1,87 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
-import { blogPosts } from "../_mocks_/BlogsData";
+import useFetch from "../hooks/useFetch";
+// import { blogPosts } from "../_mocks_/BlogsData";
 
 const Blog = () => {
+  const { isLoading, error, data } = useFetch(
+    "http://localhost:1337/api/blogs"
+  );
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error: {error.message}</h1>;
+  
   return (
-    <div className=" flex justify-center items-center">
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 max-w-screen-lg">
-        {blogPosts.map((post) => (
-          <div
-            key={post.id}
-            className="flex flex-wrap overflow-hidden rounded-lg shadow transition hover:shadow-lg p-8 min-w-64 justify-center flex-col items-center"
+
+    <div className="flex justify-center items-center">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 max-w-screen-lg">
+      {data?.data?.map((post) => (
+        <div
+          key={post.id}
+          className="flex flex-wrap overflow-hidden rounded-lg shadow transition hover:shadow-lg p-8 min-w-64 justify-center flex-col items-center"
+        >
+          {post.attributes.image && (
+            <Image
+              src={post.attributes.image}
+              alt={post.attributes.BlogTitle}
+              width={270}
+              height={100}
+              className="rounded-t-lg"
+            />
+          )}
+          <time
+            dateTime={post.attributes.BlogDate}
+            className="block text-xs text-gray-500 m-1"
           >
-            {post.image && (
-              <Image
-                src={post.image}
-                alt={post.title}
-                width={270}
-                height={100}
-                className="rounded-t-lg"
-              />
-            )}
-            <time
-              dateTime={post.date}
-              className="block text-xs text-gray-500 m-1"
-            >
-              {post.date}
-            </time>
-            <Link href={`/blog/${post.id}`}>
-              <h3 className="mt-0.5 text-base md:text-lg text-gray-900">
-                {post.title}
-              </h3>
-            </Link>
-            <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-              {post.content}
-            </p>
-          </div>
-        ))}
-      </div>
+            {new Date(post.attributes.BlogDate).toLocaleDateString()}
+          </time>
+          <Link href={`/blog/${post.id}`}>
+            <h3 className="mt-0.5 text-base md:text-lg text-gray-900">
+              {post.attributes.BlogTitle}
+            </h3>
+          </Link>
+          <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+            {post.attributes.BlogText}
+          </p>
+        </div>
+      ))}
     </div>
+  </div>
+    // <div className=" flex justify-center items-center">
+    //   <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 max-w-screen-lg">
+    //     {data?.data?.map((post) => (
+    //       <div
+    //         key={post.id}
+    //         className="flex flex-wrap overflow-hidden rounded-lg shadow transition hover:shadow-lg p-8 min-w-64 justify-center flex-col items-center"
+    //       >
+    //         {post.image && (
+    //           <Image
+    //             src={post.image}
+    //             alt={post.title}
+    //             width={270}
+    //             height={100}
+    //             className="rounded-t-lg"
+    //           />
+    //         )}
+    //         <time
+    //           dateTime={post.date}
+    //           className="block text-xs text-gray-500 m-1"
+    //         >
+    //           {post.date}
+    //         </time>
+    //         <Link href={`/blog/${post.id}`}>
+    //           <h3 className="mt-0.5 text-base md:text-lg text-gray-900">
+    //             {post.title}
+    //           </h3>
+    //         </Link>
+    //         <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+    //           {post.content}
+    //         </p>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
