70ea25599b90b36b6319205b18b851d28abb82f7
busratok <busratokuc@gmail.com>
2024-05-21 10:37:34 +0100

    Axios is installed. Global api file is created. First api call -getCatgeories- is created.

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "pics.freeicons.io",
-				port: "",
-			},
-
-			{
-				protocol: "https",
-				hostname: "**.hearstapps.com",
-				port: "",
-			},
-
-			{
-				protocol: "https",
-				hostname: "www.topgear.com",
-				port: "",
-			},
+  images: {
+    remotePatterns: [
       {
-        protocol: 'https',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "better-wealth-48913fa7ae.media.strapiapp.com",
+        port: "",
+      },
+
+      {
+        protocol: "https",
         hostname: "pics.freeicons.io",
-        pathname: '**',
+        port: "",
+      },
+
+      {
+        protocol: "https",
+        hostname: "**.hearstapps.com",
+        port: "",
+      },
+
+      {
+        protocol: "https",
+        hostname: "www.topgear.com",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: "i.pinimg.com",
-        pathname: '**',
+        protocol: "https",
+        hostname: "pics.freeicons.io",
+        pathname: "**",
       },
-		],
-  }
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
