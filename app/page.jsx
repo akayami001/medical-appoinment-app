574fc0fc1ca2f826f29d31a09c92c4bb5311d57e
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-06-06 17:37:09 +0100

    our doctors have been added

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,17 @@
+import dynamic from "next/dynamic";
 import Hero from "./_components/Hero";
 import Categories from "./_components/Categories";
 import HospitalList from "./_components/HospitalList";
 import OurServices from "./_components/OurServices";
-import Gallery from "./_components/Gallery";
 import Testimonials from "./_components/Testimonials";
 import BeforeAfterComponent from "./_components/BeforeAfterComponent";
 import WhatsAppIcon from "./_components/WhatsAppIcon";
+
+// Dynamically import MainGallery
+const MainGallery = dynamic(() => import("./_components/MainGallery"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
   return (
@@ -15,9 +21,10 @@ export default function Home() {
       <HospitalList />
       <BeforeAfterComponent />
       <Testimonials />
-      <Gallery />
+      <MainGallery title="Gallery" />
+      <MainGallery title="Our Doctors" />
       <OurServices />
-      <WhatsAppIcon/>
+      <WhatsAppIcon />
     </div>
   );
 }
