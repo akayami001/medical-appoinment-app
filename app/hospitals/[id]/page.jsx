ee4e6f27a9e1fe2335819a07bc73b4740b549f68
mehmet ali <beyzadetoprak@gmail.com>
2024-06-10 09:52:16 +0200

    hospital api changes

--- app/hospitals/[id]/page.jsx
+++ app/hospitals/[id]/page.jsx
@@ -1,5 +1,3 @@
-
-
 import Image from "next/image";
 import Link from "next/link";
 import { getHospitals } from "@/app/_utils/GlobalApi";
@@ -12,17 +10,13 @@ export async function getStaticPaths() {
   return { paths, fallback: false };
 }
 
-// export async function getStaticProps({ params }) {
-//   const data = await getHospitals();
-//   const hospital = data.find((h) => h.id === parseInt(params.id));
-//   return { props: { hospital } };
-// }
+export async function getStaticProps({ params }) {
+  const data = await getHospitals();
+  const hospital = data.find((h) => h.id === parseInt(params.id));
+  return { props: { hospital } };
+}
 
 const HospitalDetails = ({ hospital }) => {
-
-  
-
-  //error page not found
 
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
@@ -30,14 +24,11 @@ const HospitalDetails = ({ hospital }) => {
         <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
           <div className="text-center ltr:sm:text-left rtl:sm:text-right">
             <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
-              Bau Dental
+            {hospital.attributes.name}
             </h2>
 
             <p className="hidden  text-white/70 md:mt-6 md:block md:text-lg md:leading-relaxed">
-              Our health clinic is one of the Medicinity companies. Like the
-              other businesses within Medicinity, our health clinic aims to be a
-              successful and high-quality solution partner in the healthcare
-              tourism industry.
+            {hospital.attributes.description}
             </p>
 
             <div className="mt-4 sm:mt-8">
@@ -57,7 +48,7 @@ const HospitalDetails = ({ hospital }) => {
             <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
               <Image
                 alt=""
-                src="/baudental2.jpg"
+                src={hospital.attributes.image.data.attributes.url}
                 width={200}
                 height={200}
                 className="absolute inset-0 h-full w-full object-cover"
@@ -67,14 +58,7 @@ const HospitalDetails = ({ hospital }) => {
             <div className="lg:py-24">
               <h2 className="text-3xl font-bold sm:text-4xl">Overview</h2>
 
-              <p className="mt-4 text-gray-600">Our health clinic is one of the Medicinity companies. Like the
-                other businesses within Medicinity, our clinic aims to be a
-                successful and high-quality solution partner in the healthcare
-                tourism industry. We offer a full range of aesthetic operations,
-                dental treatments, and hair transplant procedures, all under the
-                supervision of professional doctors and well-trained staff. We
-                provide 24/7 support whenever you need it, both during and after
-                the procedure.</p>
+              <p className="mt-4 text-gray-600">{hospital.attributes.vision}</p>
             </div>
           </div>
         </div>
@@ -95,14 +79,7 @@ const HospitalDetails = ({ hospital }) => {
             <div className="lg:py-16">
               <h2 className="text-3xl font-bold sm:text-4xl">Why Us</h2>
 
-              <p>  Our health clinic has grown from a commitment to providing
-                differentiated healthcare solutions for our customers,
-                prioritizing quality, trust, and diversity. We are dedicated to
-                meeting and exceeding the full spectrum of our customers&apos;
-                expectations—from the comfort of their homes to their safe
-                return home—by offering high-level support and continuous care.
-                We advocate for everyone who chooses our services, helping them
-                heal, feel better, and enhance their quality of life.</p>
+              <p>{hospital.attributes.mission}</p>
             </div>
           </div>
         </div>
@@ -124,12 +101,11 @@ const HospitalDetails = ({ hospital }) => {
               <h2 className="text-3xl font-bold sm:text-4xl">Our Services</h2>
 
               <ul>
-                <li>
-                  <h6>Dental Care</h6>
-                  <h6>Dental Implants</h6>
-                  <h6>Fillings</h6>
-                  <h6>Crowns</h6>
-                </li>
+              {hospital.attributes.services.data.map((service) => (
+                  <li key={service.id}>
+                    <h6>{service.attributes.name}</h6>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
