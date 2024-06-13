f57a96aac934e7c63c324d9c7746ce225d849dd7
busratok <busratokuc@gmail.com>
2024-06-13 11:25:03 +0100

    up

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -3,17 +3,16 @@ import { revalidateTag } from "next/cache";
 export async function POST(req) {
   const body = await req.json(); // Parse the request body as JSON
   const tag = body?.model; // Extract the 'model' from the parsed body
-  console.log(tag);
   const url = new URL(req.url, `http://${req.headers.host}`);
   const secret = url.searchParams.get("secret");
 
-  if (secret !== process.env.SECRET) {
-    return Response.json({
-      revalidated: false,
-      now: Date.now(),
-      message: "Invalid Token",
-    });
-  }
+  //   if (secret !== process.env.SECRET) {
+  //     return Response.json({
+  //       revalidated: false,
+  //       now: Date.now(),
+  //       message: "Invalid Token",
+  //     });
+  //   }
   if (tag) {
     revalidateTag(tag);
     return Response.json({
